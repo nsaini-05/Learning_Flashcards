29a14af7aa118d620edcb32d4f442073c3349f6b
Neeraj <asaini7788@gmail.com>
2021-02-05 00:38:01 -0500

    Removed Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,13 @@ import React from "react";
 function Header()
 {
     return         <nav className="navbar">
-            <div className="logo"><a href="www.google.com">FlashCards</a></div>
+            <div className="logo"><a href={"home"}>FlashCards</a></div>
             
-            <a className="togglebtn" href="www.google.com"><span className="line"></span>
-                <span className="line"></span>
-                <span className="line"></span></a>
+           
             <div className="navbar-links">
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Create</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href={"home"}>Home</a></li>
+                    <li><a href={"about"}>About</a></li>
                 </ul>
             </div>
             
